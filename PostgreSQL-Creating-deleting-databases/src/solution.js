303b0025ed04f66df03beace641972b99920b731
Adan Rodriguez-Jones <138242165+Adanjones@users.noreply.github.com>
2024-04-19 18:06:17 -0400

    Initial upload

---
+++ PostgreSQL-Creating-deleting-databases/src/solution.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: "dpg-coe4h9a0si5c739arcig-a.ohio-postgres.render.com",
+  user: "dev_jn4u_user",
+  database: "dev_jn4u",
+  password: "32k9f7xYVWQL9yy6tEkRVxU9d6t7JGuF",
+};
