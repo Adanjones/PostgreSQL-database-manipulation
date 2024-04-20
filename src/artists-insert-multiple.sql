f499ff8633afa8b85628131b225414b1c9edb9fd
Adan <jonesadan@yahoo.com>
2024-04-19 22:28:06 -0400

    finished updating artists requirements

---
+++ src/artists-insert-multiple.sql
@@ -0,0 +1,26 @@
+INSERT INTO artists
+  (artist_name, birthday, birthplace, is_alive)
+  VALUES (
+    'Seth Rogen',
+    '1982-04-15',
+    'Vancouver, Canada',
+    'true'
+  ),
+  (
+    'Vincent Van Gogh',
+    '1853-03-30',
+    'Zundery, Netherlands',
+    'false'
+  ),
+  (
+    'Frida Kahlo',
+    '1907-07-06',
+    'Mexico City, Mexico',
+    'false'
+  ),
+  (
+    'Lenoardo da Vinci',
+    '1452-04-15',
+    'vinci, Italy',
+    'false'
+  )
